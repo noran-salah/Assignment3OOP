15c4d78ec0515d1e5daa0c175bea002ccab34986
Noran Salah <noransalah840@gmail.com>
2025-01-19 02:04:59 +0200

    Q4

--- Assignment3OOP/Program.cs
+++ Assignment3OOP/Program.cs
@@ -36,7 +36,16 @@
             // Answer => c) Only if they are static
             #endregion
 
-          
+            #region Q4
+            //In C#, can an interface inherit from another interface?
+            //a) No, interfaces cannot inherit from each other
+            //b) Yes, interfaces can inherit from multiple interfaces
+            //c) Yes, but only if they have the same methods
+            //d) Only if the interfaces are in the same namespace
+
+            // Answer => b) Yes, interfaces can inherit from multiple interfaces
+            #endregion
+
 
 
 
