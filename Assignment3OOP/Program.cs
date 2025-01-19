f898db5d301a323a53af3d135bbbe552839346ab
Noran Salah <noransalah840@gmail.com>
2025-01-19 02:11:35 +0200

    Q9

--- Assignment3OOP/Program.cs
+++ Assignment3OOP/Program.cs
@@ -85,6 +85,18 @@
             // Answer => b) To provide a clear separation between interface and class members
             #endregion
 
+            #region Q9
+            //In C#, can an interface have a constructor?
+            //a) Yes, but it must be private
+            //b) No, interfaces cannot have constructors
+            //c) Yes, but only if the interface is sealed
+            //d) Only if the constructor is static
+
+            // Answer => d) Only if the constructor is static
+            #endregion
+
+
+
 
 
 
