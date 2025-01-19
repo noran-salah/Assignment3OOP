498a12c4adedc1ac40d8f7d98d4376ef0a94cc46
Noran Salah <noransalah840@gmail.com>
2025-01-19 02:03:26 +0200

    Q1

--- Assignment3OOP/Program.cs
+++ Assignment3OOP/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Q1
+
+            //Question 1:
+            //What is the primary purpose of an interface in C#?
+            //a) To provide a way to implement multiple inheritance
+            //b) To define a blueprint for a class
+            //c) To declare abstract methods and properties
+            //d) To create instances of objects
+
+            // Answer => b) To define a blueprint for a class
+            #endregion
+
+
         }
     }
 }
